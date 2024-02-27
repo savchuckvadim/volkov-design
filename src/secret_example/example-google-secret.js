be6c436a4b4c9d6701fbd48a16f9aa924a0e2a8c
savchuckvadim <savchuckvadim@gmail.com>
2024-02-27 17:08:22 +0100

    initial

---
+++ src/secret_example/example-google-secret.js
@@ -0,0 +1,2 @@
+export const TOKEN = 'token'
+export const APRIL_DOCS_TOKEN = 'token'
---
+++ src/secret_example/secret.js
@@ -0,0 +1,10 @@
+
+export const secretFirebase = {
+    apiKey: "AIz...;9PFLs",
+    authDomain: "a....com",
+    projectId: "a...",
+    storageBucket: "a...t.com",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
